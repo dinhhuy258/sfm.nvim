ee052df45d2fd6dfe46cd0fd3930f22be768bb78
Huy Duong <huy.duongdinh@gmail.com>
2024-06-26 17:50:47 +0700

    fix: update colors issue in neovim version 10

--- lua/sfm/colors.lua
+++ lua/sfm/colors.lua
@@ -1,78 +1,59 @@
 local M = {}
 
-local function get_color_from_hl(hl_name, fallback)
-  local id = vim.api.nvim_get_hl_id_by_name(hl_name)
-  if not id then
-    return fallback
+local function create_highlight_group(hl_group_name, link_to_if_exists, fg, bg, gui)
+  local success, hl_group = pcall(vim.api.nvim_get_hl_by_name, hl_group_name, true)
+  if not success or not hl_group.foreground or not hl_group.background then
+    for _, link_to in ipairs(link_to_if_exists) do
+      success, hl_group = pcall(vim.api.nvim_get_hl_by_name, link_to, true)
+      if success then
+        local new_group_has_settings = bg or fg or gui
+        local link_to_has_settings = hl_group.foreground or hl_group.background
+        if link_to_has_settings or not new_group_has_settings then
+          vim.cmd("highlight default link " .. hl_group_name .. " " .. link_to)
+
+          return
+        end
+      end
+    end
+
+    local cmd = "highlight default " .. hl_group_name
+    if bg then
+      cmd = cmd .. " guibg=#" .. bg
+    end
+    if fg then
+      cmd = cmd .. " guifg=#" .. fg
+    else
+      cmd = cmd .. " guifg=NONE"
+    end
+    if gui then
+      cmd = cmd .. " gui=" .. gui
+    end
+    vim.cmd(cmd)
   end
-
-  local foreground = vim.fn.synIDattr(vim.fn.synIDtrans(id), "fg")
-  if not foreground or foreground == "" then
-    return fallback
-  end
-
-  return foreground
-end
-
-local function get_colors()
-  return {
-    red = vim.g.terminal_color_1 or get_color_from_hl("Keyword", "Red"),
-    green = vim.g.terminal_color_2 or get_color_from_hl("Character", "Green"),
-    yellow = vim.g.terminal_color_3 or get_color_from_hl("PreProc", "Yellow"),
-    blue = vim.g.terminal_color_4 or get_color_from_hl("Include", "Blue"),
-    purple = vim.g.terminal_color_5 or get_color_from_hl("Define", "Purple"),
-    cyan = vim.g.terminal_color_6 or get_color_from_hl("Conditional", "Cyan"),
-    dark_red = vim.g.terminal_color_9 or get_color_from_hl("Keyword", "DarkRed"),
-    orange = vim.g.terminal_color_11 or get_color_from_hl("Number", "Orange"),
-  }
-end
-
-local function get_hl_groups()
-  local colors = get_colors()
-
-  return {
-    SFMRootFolder = { fg = colors.purple },
-    SFMSymlink = { gui = "bold", fg = colors.cyan },
-    SFMFileIndicator = { fg = "#3b4261" },
-    SFMFolderIndicator = { fg = "#3b4261" },
-    SFMSelection = { fg = colors.blue },
-  }
-end
-
-local function get_links()
-  return {
-    SFMFolderName = "Directory",
-    SFMFolderIcon = "Directory",
-    SFMDefaultFileIcon = "Normal",
-    SFMFileName = "Normal",
-
-    -- win option
-    SFMNormal = "Normal",
-    SFMNormalNC = "NormalNC",
-    SFMEndOfBuffer = "EndOfBuffer",
-    SFMCursorLine = "CursorLine",
-    SFMCursorLineNr = "CursorLineNr",
-    SFMLineNr = "LineNr",
-    SFMWinSeparator = "WinSeparator",
-    SFMStatusLine = "StatusLine",
-    SFMStatuslineNC = "StatuslineNC",
-    SFMSignColumn = "SignColumn",
-  }
 end
 
 function M.setup()
-  local higlight_groups = get_hl_groups()
-  for k, d in pairs(higlight_groups) do
-    local gui = d.gui and " gui=" .. d.gui or ""
-    local fg = d.fg and " guifg=" .. d.fg or ""
-    local bg = d.bg and " guibg=" .. d.bg or ""
-    vim.api.nvim_command("hi def " .. k .. gui .. fg .. bg)
-  end
+  create_highlight_group("SFMRootFolder", {}, nil, nil, "bold,italic")
+  create_highlight_group("SFMSymlink", { "Underlined" }, nil, nil, nil)
+  create_highlight_group("SFMFileIndicator", {}, "3b4261", nil, nil)
+  create_highlight_group("SFMFolderIndicator", {}, "3b4261", nil, nil)
+  create_highlight_group("SFMSelection", {}, nil, nil, nil)
 
-  local links = get_links()
-  for k, d in pairs(links) do
-    vim.api.nvim_command("hi def link " .. k .. " " .. d)
-  end
+  create_highlight_group("SFMFolderName", { "Directory" }, nil, nil, nil)
+  create_highlight_group("SFMFolderIcon", { "Directory" }, nil, nil, nil)
+  create_highlight_group("SFMDefaultFileIcon", { "Normal" }, nil, nil, nil)
+  create_highlight_group("SFMFileName", { "Normal" }, nil, nil, nil)
+
+  create_highlight_group("SFMNormal", { "Normal" }, nil, nil, nil)
+  create_highlight_group("SFMNormalNC", { "NormalNC" }, nil, nil, nil)
+  create_highlight_group("SFMEndOfBuffer", { "EndOfBuffer" }, nil, nil, nil)
+  create_highlight_group("SFMCursorLine", { "CursorLine" }, nil, nil, nil)
+  create_highlight_group("SFMCursorLineNr", { "CursorLineNr" }, nil, nil, nil)
+  create_highlight_group("SFMLineNr", { "LineNr" }, nil, nil, nil)
+  create_highlight_group("SFMWinSeparator", { "WinSeparator" }, nil, nil, nil)
+  create_highlight_group("SFMStatusLine", { "StatusLine" }, nil, nil, nil)
+  create_highlight_group("SFMStatuslineNC", { "StatuslineNC" }, nil, nil, nil)
+  create_highlight_group("SFMSignColumn", { "SignColumn" }, nil, nil, nil)
 end
 
 return M
